a508383b64685220856d2737921cd14b01d88551
Tuan Luu <tuan.luu@asnet.com.vn>
2025-03-24 20:58:40 +0700

    Refactor aggregateroot for tracking events

--- Wpm.SharedKernel/AggregateRoot.cs
+++ Wpm.SharedKernel/AggregateRoot.cs
@@ -2,4 +2,29 @@
 
 public abstract class AggregateRoot : Entity
 {
+    private readonly List<IDomainEvent> changes = new();
+
+    public int Version { get; private set; }
+    
+    public IReadOnlyList<IDomainEvent> GetChanges() => changes.AsReadOnly();
+
+    public void ClearChanges() => changes.Clear();
+
+    protected void ApplyDomainEvent(IDomainEvent domainEvent)
+    {
+        ChangeStateByUsingDomainEvent(domainEvent);
+        changes.Add(domainEvent);
+        Version++;
+    }
+
+    public void Load(IEnumerable<IDomainEvent> history)
+    {
+        foreach (var domainEvent in history)
+        {
+            ApplyDomainEvent(domainEvent);
+        }
+        ClearChanges();
+    }
+
+    protected abstract void ChangeStateByUsingDomainEvent(IDomainEvent domainEvent);
 }
